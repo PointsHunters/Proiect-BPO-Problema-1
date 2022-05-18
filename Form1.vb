bf5fd8df21a6f15bf951c175ae78d4d196f71581
DANIEL-PETRE MANGUTA <manguta.da.danielpetre21@student.rau.ro>
2022-05-18 08:39:53 +0300

    Fully functional login

--- Form1.vb
+++ Form1.vb
@@ -15,83 +15,88 @@
 
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
         i1 = Me.ComboBox1.SelectedIndex
-        semn = "nu"
-        If i1 = 0 Then
-            For i2 = 0 To Form2.ComboBox1.Items.Count - 1
-                If Me.TextBox1.Text = Form2.ComboBox1.Items.Item(i2) Then
-                    semn = "ok"
-                    If Me.TextBox2.Text = Form2.ComboBox2.Items.Item(i2) Then
-                        MsgBox("am inceput prelucrarea")
+    End Sub
 
-                        Me.Hide()
-                        Form2.Show()
-                    Else
-                        MsgBox("parola este gresita")
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        semn = "nu"
+        If (String.IsNullOrEmpty(Me.ComboBox1.Text)) Then
+            MsgBox("Selecteaza grupul")
+        Else
+            If (String.IsNullOrEmpty(Me.TextBox1.Text)) And (String.IsNullOrEmpty(Me.TextBox2.Text)) Then
+                MsgBox("Trebuie sa introduci un utilizator si o parola")
+            Else
+                If (String.IsNullOrEmpty(Me.TextBox1.Text)) Then
+                    MsgBox("Trebuie sa introduci un utilizator")
+                ElseIf (String.IsNullOrEmpty(Me.TextBox2.Text)) Then
+                    MsgBox("Trebuie sa introduci o parola")
+                Else
+                    If i1 = 0 Then
+                        For i2 = 0 To Form2.ComboBox1.Items.Count - 1
+                            If Me.TextBox1.Text = Form2.ComboBox1.Items.Item(i2) Then
+                                semn = "ok"
+                                If Me.TextBox2.Text = Form2.ComboBox2.Items.Item(i2) Then
+                                    MsgBox("Am inceput prelucrarea")
+
+                                    Me.Hide()
+                                    Form2.Show()
+                                Else
+                                    MsgBox("Parola este gresita")
+                                End If
+                            End If
+                        Next
+                    End If
+
+                    If i1 = 1 Then
+                        For i2 = 0 To Form2.ComboBox3.Items.Count - 1
+                            If Me.TextBox1.Text = Form2.ComboBox3.Items.Item(i2) Then
+                                semn = "ok"
+                                If Me.TextBox2.Text = Form2.ComboBox4.Items.Item(i2) Then
+                                    MsgBox("Am inceput prelucrarea")
+                                    Me.Hide()
+                                    Form2.Show()
+                                Else
+                                    MsgBox("Parola este gresita")
+                                End If
+                            End If
+                        Next
+                    End If
+
+                    If i1 = 2 Then
+                        For i2 = 0 To Form2.ComboBox5.Items.Count - 1
+                            If Me.TextBox1.Text = Form2.ComboBox5.Items.Item(i2) Then
+                                semn = "ok"
+                                If Me.TextBox2.Text = Form2.ComboBox6.Items.Item(i2) Then
+                                    MsgBox("Am inceput prelucrarea")
+
+                                    Me.Hide()
+                                    Form2.Show()
+                                Else
+                                    MsgBox("Parola este gresita")
+                                End If
+                            End If
+                        Next
+                    End If
+
+                    If i1 = 3 Then
+                        For i2 = 0 To Form2.ComboBox7.Items.Count - 1
+                            If Me.TextBox1.Text = Form2.ComboBox7.Items.Item(i2) Then
+                                semn = "ok"
+                                If Me.TextBox2.Text = Form2.ComboBox8.Items.Item(i2) Then
+                                    MsgBox("Am inceput prelucrarea")
+
+                                    Me.Hide()
+                                    Form2.Show()
+                                Else
+                                    MsgBox("Parola este gresita")
+                                End If
+                            End If
+                        Next
+                    End If
+                    If semn = "nu" Then
+
+                        MsgBox("Utilizator incorect")
                     End If
                 End If
-            Next
-            If semn = "nu" Then
-
-                MsgBox("utilizator incorect")
-            End If
-        End If
-        semn = "nu"
-        If i1 = 1 Then
-            For i2 = 0 To Form2.ComboBox3.Items.Count - 1
-                If Me.TextBox1.Text = Form2.ComboBox3.Items.Item(i2) Then
-                    semn = "ok"
-                    If Me.TextBox2.Text = Form2.ComboBox4.Items.Item(i2) Then
-                        MsgBox("am inceput prelucrarea")
-                        Me.Hide()
-                        Form2.Show()
-                    Else
-                        MsgBox("parola este gresita")
-                    End If
-                End If
-            Next
-            If semn = "nu" Then
-
-                MsgBox("utilizator incorect")
-            End If
-        End If
-        semn = "nu"
-        If i1 = 2 Then
-            For i2 = 0 To Form2.ComboBox5.Items.Count - 1
-                If Me.TextBox1.Text = Form2.ComboBox5.Items.Item(i2) Then
-                    semn = "ok"
-                    If Me.TextBox2.Text = Form2.ComboBox6.Items.Item(i2) Then
-                        MsgBox("am inceput prelucrarea")
-
-                        Me.Hide()
-                        Form2.Show()
-                    Else
-                        MsgBox("parola este gresita")
-                    End If
-                End If
-            Next
-            If semn = "nu" Then
-
-                MsgBox("utilizator incorect")
-            End If
-        End If
-        semn = "nu"
-        If i1 = 3 Then
-            For i2 = 0 To Form2.ComboBox7.Items.Count - 1
-                If Me.TextBox1.Text = Form2.ComboBox7.Items.Item(i2) Then
-                    semn = "ok"
-                    If Me.TextBox2.Text = Form2.ComboBox8.Items.Item(i2) Then
-                        MsgBox("am inceput prelucrarea")
-
-                        Me.Hide()
-                        Form2.Show()
-                    Else
-                        MsgBox("parola este gresita")
-                    End If
-                End If
-            Next
-            If semn = "nu" Then
-
-                MsgBox("utilizator incorect")
             End If
         End If
     End Sub
--- Form2.Designer.vb
+++ Form2.Designer.vb
@@ -50,9 +50,10 @@ Partial Class Form2
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(713, 415)
+        Me.Button1.Location = New System.Drawing.Point(815, 553)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(86, 31)
         Me.Button1.TabIndex = 4
         Me.Button1.Text = "Exit"
         Me.Button1.UseVisualStyleBackColor = True
@@ -60,203 +61,222 @@ Partial Class Form2
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(130, 94)
+        Me.Label1.Location = New System.Drawing.Point(149, 125)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(60, 15)
+        Me.Label1.Size = New System.Drawing.Size(75, 20)
         Me.Label1.TabIndex = 5
         Me.Label1.Text = "Username"
         '
         'ComboBox1
         '
         Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(130, 112)
+        Me.ComboBox1.Items.AddRange(New Object() {"G1U1", "G1U2", "G1U3"})
+        Me.ComboBox1.Location = New System.Drawing.Point(149, 149)
+        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(121, 23)
+        Me.ComboBox1.Size = New System.Drawing.Size(138, 28)
         Me.ComboBox1.TabIndex = 6
         '
         'ComboBox2
         '
         Me.ComboBox2.FormattingEnabled = True
-        Me.ComboBox2.Location = New System.Drawing.Point(257, 112)
+        Me.ComboBox2.Items.AddRange(New Object() {"G1U1P", "G1U2P", "G1U3P"})
+        Me.ComboBox2.Location = New System.Drawing.Point(294, 149)
+        Me.ComboBox2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.ComboBox2.Name = "ComboBox2"
-        Me.ComboBox2.Size = New System.Drawing.Size(121, 23)
+        Me.ComboBox2.Size = New System.Drawing.Size(138, 28)
         Me.ComboBox2.TabIndex = 8
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(257, 94)
+        Me.Label2.Location = New System.Drawing.Point(294, 125)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(57, 15)
+        Me.Label2.Size = New System.Drawing.Size(70, 20)
         Me.Label2.TabIndex = 7
         Me.Label2.Text = "Password"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(75, 115)
+        Me.Label3.Location = New System.Drawing.Point(86, 153)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(49, 15)
+        Me.Label3.Size = New System.Drawing.Size(62, 20)
         Me.Label3.TabIndex = 9
         Me.Label3.Text = "Group 1"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(75, 144)
+        Me.Label4.Location = New System.Drawing.Point(86, 192)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(49, 15)
+        Me.Label4.Size = New System.Drawing.Size(62, 20)
         Me.Label4.TabIndex = 14
         Me.Label4.Text = "Group 2"
         '
         'ComboBox3
         '
         Me.ComboBox3.FormattingEnabled = True
-        Me.ComboBox3.Location = New System.Drawing.Point(130, 141)
+        Me.ComboBox3.Location = New System.Drawing.Point(149, 188)
+        Me.ComboBox3.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.ComboBox3.Name = "ComboBox3"
-        Me.ComboBox3.Size = New System.Drawing.Size(121, 23)
+        Me.ComboBox3.Size = New System.Drawing.Size(138, 28)
         Me.ComboBox3.TabIndex = 13
         '
         'ComboBox4
         '
         Me.ComboBox4.FormattingEnabled = True
-        Me.ComboBox4.Location = New System.Drawing.Point(257, 141)
+        Me.ComboBox4.Location = New System.Drawing.Point(294, 188)
+        Me.ComboBox4.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.ComboBox4.Name = "ComboBox4"
-        Me.ComboBox4.Size = New System.Drawing.Size(121, 23)
+        Me.ComboBox4.Size = New System.Drawing.Size(138, 28)
         Me.ComboBox4.TabIndex = 11
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(75, 173)
+        Me.Label5.Location = New System.Drawing.Point(86, 231)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(49, 15)
+        Me.Label5.Size = New System.Drawing.Size(62, 20)
         Me.Label5.TabIndex = 17
         Me.Label5.Text = "Group 3"
         '
         'ComboBox5
         '
         Me.ComboBox5.FormattingEnabled = True
-        Me.ComboBox5.Location = New System.Drawing.Point(130, 170)
+        Me.ComboBox5.Location = New System.Drawing.Point(149, 227)
+        Me.ComboBox5.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.ComboBox5.Name = "ComboBox5"
-        Me.ComboBox5.Size = New System.Drawing.Size(121, 23)
+        Me.ComboBox5.Size = New System.Drawing.Size(138, 28)
         Me.ComboBox5.TabIndex = 16
         '
         'ComboBox6
         '
         Me.ComboBox6.FormattingEnabled = True
-        Me.ComboBox6.Location = New System.Drawing.Point(257, 170)
+        Me.ComboBox6.Location = New System.Drawing.Point(294, 227)
+        Me.ComboBox6.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.ComboBox6.Name = "ComboBox6"
-        Me.ComboBox6.Size = New System.Drawing.Size(121, 23)
+        Me.ComboBox6.Size = New System.Drawing.Size(138, 28)
         Me.ComboBox6.TabIndex = 15
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(75, 202)
+        Me.Label6.Location = New System.Drawing.Point(86, 269)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(49, 15)
+        Me.Label6.Size = New System.Drawing.Size(62, 20)
         Me.Label6.TabIndex = 20
         Me.Label6.Text = "Group 4"
         '
         'ComboBox7
         '
         Me.ComboBox7.FormattingEnabled = True
-        Me.ComboBox7.Location = New System.Drawing.Point(130, 199)
+        Me.ComboBox7.Location = New System.Drawing.Point(149, 265)
+        Me.ComboBox7.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.ComboBox7.Name = "ComboBox7"
-        Me.ComboBox7.Size = New System.Drawing.Size(121, 23)
+        Me.ComboBox7.Size = New System.Drawing.Size(138, 28)
         Me.ComboBox7.TabIndex = 19
         '
         'ComboBox8
         '
         Me.ComboBox8.FormattingEnabled = True
-        Me.ComboBox8.Location = New System.Drawing.Point(257, 199)
+        Me.ComboBox8.Location = New System.Drawing.Point(294, 265)
+        Me.ComboBox8.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.ComboBox8.Name = "ComboBox8"
-        Me.ComboBox8.Size = New System.Drawing.Size(121, 23)
+        Me.ComboBox8.Size = New System.Drawing.Size(138, 28)
         Me.ComboBox8.TabIndex = 18
         '
         'ListBox1
         '
         Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 15
-        Me.ListBox1.Location = New System.Drawing.Point(384, 112)
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(439, 149)
+        Me.ListBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(120, 109)
+        Me.ListBox1.Size = New System.Drawing.Size(137, 144)
         Me.ListBox1.TabIndex = 21
         '
         'ListBox2
         '
         Me.ListBox2.FormattingEnabled = True
-        Me.ListBox2.ItemHeight = 15
-        Me.ListBox2.Location = New System.Drawing.Point(510, 112)
+        Me.ListBox2.ItemHeight = 20
+        Me.ListBox2.Location = New System.Drawing.Point(583, 149)
+        Me.ListBox2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.ListBox2.Name = "ListBox2"
-        Me.ListBox2.Size = New System.Drawing.Size(120, 109)
+        Me.ListBox2.Size = New System.Drawing.Size(137, 144)
         Me.ListBox2.TabIndex = 22
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(636, 112)
+        Me.Button2.Location = New System.Drawing.Point(727, 149)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.Size = New System.Drawing.Size(86, 31)
         Me.Button2.TabIndex = 23
         Me.Button2.Text = "Add"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(636, 141)
+        Me.Button3.Location = New System.Drawing.Point(727, 188)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.Size = New System.Drawing.Size(86, 31)
         Me.Button3.TabIndex = 24
         Me.Button3.Text = "Add"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(636, 170)
+        Me.Button4.Location = New System.Drawing.Point(727, 227)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.Size = New System.Drawing.Size(86, 31)
         Me.Button4.TabIndex = 25
         Me.Button4.Text = "Add"
         Me.Button4.UseVisualStyleBackColor = True
         '
         'Button5
         '
-        Me.Button5.Location = New System.Drawing.Point(636, 199)
+        Me.Button5.Location = New System.Drawing.Point(727, 265)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.Size = New System.Drawing.Size(86, 31)
         Me.Button5.TabIndex = 26
         Me.Button5.Text = "Add"
         Me.Button5.UseVisualStyleBackColor = True
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(384, 227)
+        Me.TextBox1.Location = New System.Drawing.Point(439, 303)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(120, 23)
+        Me.TextBox1.Size = New System.Drawing.Size(137, 27)
         Me.TextBox1.TabIndex = 27
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(510, 227)
+        Me.TextBox2.Location = New System.Drawing.Point(583, 303)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(120, 23)
+        Me.TextBox2.Size = New System.Drawing.Size(137, 27)
         Me.TextBox2.TabIndex = 28
         '
         'Button6
         '
-        Me.Button6.Location = New System.Drawing.Point(384, 256)
+        Me.Button6.Location = New System.Drawing.Point(439, 341)
+        Me.Button6.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Button6.Name = "Button6"
-        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.Size = New System.Drawing.Size(86, 31)
         Me.Button6.TabIndex = 29
         Me.Button6.Text = "Save"
         Me.Button6.UseVisualStyleBackColor = True
         '
         'Form2
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(914, 600)
         Me.Controls.Add(Me.Button6)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
@@ -281,6 +301,7 @@ Partial Class Form2
         Me.Controls.Add(Me.ComboBox1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "Form2"
         Me.Text = "Form2"
         Me.ResumeLayout(False)
